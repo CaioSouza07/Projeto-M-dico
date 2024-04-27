8bcec6921daf72fed515ded0ea9665eb41c61f03
Caio de Souza <caiodesouza.cds@gmail.com>
2024-04-27 15:33:08 -0300

    Modificando template html

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-import templates
+
 
 # Create your views here.
 
 def cadastro(request):
 
-    return HttpResponse('Ola')
+    return render(request, 'cadastro.html')
