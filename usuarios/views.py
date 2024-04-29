f70c0ace6232f4ce3111efcbebd9eb75c5693791
Caio de Souza <caiodesouza.cds@gmail.com>
2024-04-29 10:14:23 -0300

    Modificando para autenticação

--- usuarios/views.py
+++ usuarios/views.py
@@ -7,4 +7,12 @@ from django.http import HttpResponse
 def cadastro(request):
     if request.method == "GET":
         return render(request, 'cadastro.html')
+    elif request.method == "POST":
+
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        senha = request.POST.get('senha')
+        confirmar_senha = request.POST.get('confirmar_senha')
+        
+        return HttpResponse("Deu certinho")
 
