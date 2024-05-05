6837c950c994537ae2fd0e489c32b720710a7f18
Caio de Souza <caiodesouza.cds@gmail.com>
2024-05-04 21:32:14 -0300

    Adicionando tabela do banco para medico no models.py

--- medico/admin.py
+++ medico/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Especialidades
+from .models import Especialidades , DadosMedico
 # Register your models here.
 
-admin.site.register(Especialidades)
+admin.site.register(Especialidades )
+admin.site.register(DadosMedico)
 
 
